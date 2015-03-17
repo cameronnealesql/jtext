3c140563e4b4abecccf13c4faa74dd4f9ff91dbd
Cameron Neale <cameronneale@outlook.com>
2015-03-18 00:08:35 +0900

    Update JDATE.sql

--- JDATE.sql
+++ JDATE.sql
@@ -9,10 +9,16 @@ BEGIN
           @month AS INT = MONTH(@date),
           @day AS INT = DAY(@date),
           @result AS NVARCHAR(10) = N'';
-  IF @year > 1989 OR (@year = 1989 AND @day >= 8)
-    SET @result += CONCAT(N'平成', @year - 1988)
-  ELSE IF @year > 1925
-    SET @result += CONCAT(N'昭和', @year - 1925);
+          
+-- The logic is fine, but calculations can be simplified by using DATE rather than YEAR MONTH DAY separately
+       IF @year > 1989 OR (@year = 1989 AND @month>= 1  AND @day >= 8)
+            SET @result += CONCAT(N'平成', @year - 1988)
+  ELSE IF @year > 1926 OR (@year = 1926 AND @month = 12 AND @day >= 25)
+            SET @result += CONCAT(N'昭和', @year - 1925)
+  ELSE IF @year > 1912 OR (@year = 1912 AND @month = 7 AND @day >= 30)
+            SET @result += CONCAT(N'大正', @year - 1911);
+  ELSE IF @year > 1868 OR (@year = 1868 AND @month = 9 AND @day >= 8)
+            SET @result += CONCAT(N'明治', @year - 1867);
   SET @result += CONCAT(N'年', @month, N'月', @day, N'日');
   RETURN @result;
 END;

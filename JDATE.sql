f3f6120011a41d31dba1ad3d70c93dcae1454493
Cameron Neale <cameronneale@outlook.com>
2015-03-19 00:32:35 +0900

    Update JDATE.sql
    Fixed typo

--- JDATE.sql
+++ JDATE.sql
@@ -12,11 +12,13 @@ BEGIN
   ELSE IF @date >= '19261225'
             SET @result += CONCAT(N'昭和', YEAR(@date) - 1925)
   ELSE IF @date >= '19120730'
-            SET @result += CONCAT(N'大正', YEAR(@date) - 1911);
+            SET @result += CONCAT(N'大正', YEAR(@date) - 1911)
   ELSE IF @date >= '18680908'
             SET @result += CONCAT(N'明治', YEAR(@date) - 1867);
 
   SET @result += CONCAT(N'年', MONTH(@date), N'月', DAY(@date), N'日');
+  
   RETURN @result;
+  
 END;
 GO

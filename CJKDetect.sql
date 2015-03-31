b71050cd7d9a3d7152990b1a6fce995fabf1324c
Cameron Neale <cameronneale@outlook.com>
2015-04-01 00:11:48 +0900

    Update CJKDetect.sql

--- CJKDetect.sql
+++ CJKDetect.sql
@@ -1,3 +1,6 @@
 --This UDF will detect whether or not a string contains certain CJK characters in a string of text
 CREATE FUNCTION JTEXT.CJKDetect(text AS NVARCHAR(MAX))
-  RETURNS BOOLEAN;
+  RETURNS BOOLEAN
+  AS
+  
+  ;
